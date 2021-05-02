b9c91eb306d9419ffd8c2c0f6e622072e1c5a78a
toddteller <giovi.designer@gmail.com>
2021-05-02 13:11:40 +0200

    First commit

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+const Navbar = () => (
+    <div>
+        <Image src="/images/Logo.svg"
+         height="100" width="100">
+         
+        </Image>
+        <h1> Navigation Bar </h1>
+    </div>
+)
+
+export default Navbar
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from "../components/Navbar"
+
+
+const Home = () => (
+  <div>
+    <Navbar></Navbar>
+    <h1> TEST</h1>
+  </div>
+)
+
+export default Home
